0faf2f819fe850dde09947b1f4f09874b4211d6f
monsterstudios <info@monsterstudios.com.ng>
2020-06-03 16:01:39 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -19,6 +19,26 @@ $(function() {
                 }
             });
 
+            /**
+             * Vertically center Bootstrap 3 modals so they aren't always stuck at the top
+             */
+            function reposition() {
+                var modal = $(this),
+                    dialog = modal.find('.modal-dialog');
+                modal.css('display', 'block');
+
+                // Dividing by two centers the modal exactly, but dividing by three 
+                // or four works better for larger screens.
+                dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+            }
+            // Reposition when a modal is shown
+            $('.modal').on('show.bs.modal', reposition);
+            // Reposition when the window is resized
+            $(window).on('resize', function() {
+                $('.modal:visible').each(reposition);
+            });
+
+
             $('.editor').click(function() {
                 $(this).each(function(n) {
                     $(this).closest('.form-edit').find('input').removeAttr('readonly');
